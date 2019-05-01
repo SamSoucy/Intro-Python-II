037e4d02c2f66c373ff35abdda24a317583019f2
Samsoucy <newf930@outlook.com>
2019-05-01 17:52:16 -0400

    adding player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, curt_room):
+        self.name = name
+        self.curt_room = curt_room
+        self.items = []
+    def move(self, direction):
+        if direction in ["n", "e", "s", "w"]:
+            move_rooms = self.curt_room.that_room(direction)
+            if move_rooms is not None:
+                self.curt_room = move_rooms
+                print(self.curt_room)
+            else:
+                print("Cannot go in that direction.")
